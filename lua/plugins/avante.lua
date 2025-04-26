fadc8d1c714b23ede6516749e4d1d7f29b490d43
Caio Henrique <167234050+CHDevSec@users.noreply.github.com>
2025-04-25 23:54:28 -0300

    Add files via upload

---
+++ lua/plugins/avante.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "AvanteAI/avante.nvim",
+    config = function()
+      require("avante").setup({
+        provider = "openai",
+        openai = {
+          api_key = os.getenv("OPENAI_API_KEY"),
+          model = "gpt-4o",
+        }
+      })
+    end
+  }
+}
